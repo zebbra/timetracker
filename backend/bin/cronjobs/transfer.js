d9aee0495a87de0f4d22a41ee796abd325855882
Michael <michael.baertschi@zebbra.ch>
2020-06-10 14:46:46 +0200

    Add yarn scripts for cronjobs

--- backend/bin/cronjobs/transfer.js
+++ backend/bin/cronjobs/transfer.js
@@ -1,130 +1,17 @@
-const schedule = require("node-schedule");
-const debug = require("debug")("cronjobs");
-const path = require("path");
+const debug = require("debug")("cronjobs:transfer");
 const moment = require("moment-timezone");
-const async = require("async");
+const Async = require("async");
 const _ = require("lodash");
 
-const backup = require("./database/backup");
-const app = require(path.resolve(__dirname, "../server/server"));
-const reporting = require(path.resolve(__dirname, "../common/reporting"));
-const formatters = require(path.resolve(__dirname, "../common/formatters"));
+const app = require("../../server/server");
+const reporting = require("../../common/reporting");
+const formatters = require("../../common/formatters");
 
-const YEAR_MODELS = ["employment-profile", "setpoint"];
-const DATE_MODELS = ["track", "holiday"];
-const END_MODELS = ["employment"];
-
-const closeModel = (model, query, callback) => {
-  const Model = app.models[model];
-
-  const fields = {
-    closed: true
-  };
-
-  debug(
-    `job=closeModel going to close ${model} for query=${JSON.stringify(query)}`
-  );
-  Model.update(query, fields, (err, info) => {
-    if (err) {
-      debug(
-        `job=closeModel model=${model}s status=failed error=${err} nextInvocation=${closeAllModelsJob.nextInvocation()}`
-      );
-    } else {
-      debug(
-        `job=closeModel closed count=${
-          info.count
-        } ${model}s nextInvocation=${closeAllModelsJob.nextInvocation()}`
-      );
-    }
-
-    setTimeout(() => callback(err), 2000);
-  });
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-const closeAllModels = () => {
-  const models = [
-    "employment-profile",
-    "employment",
-    "track",
-    "holiday",
-    "setpoint"
-  ];
-  const successfull = [];
-  const failed = [];
-
-  debug(`job=closeAllModels going to close models=${models.join(", ")}`);
-  async.eachSeries(
-    models,
-    (model, next) => {
-      const query = {
-        closed: false
-      };
-
-      if (_.includes(YEAR_MODELS, model)) {
-        _.assign(query, {
-          year: { lt: moment().year() }
-        });
-      } else if (_.includes(DATE_MODELS, model)) {
-        _.assign(query, {
-          date: { lt: moment().startOf("year") }
-        });
-      } else if (_.includes(END_MODELS, model)) {
-        _.assign(query, {
-          end: { lt: moment().startOf("year") }
-        });
-      }
-
-      closeModel(model, query, err => {
-        if (err) {
-          failed.push(model);
-        } else {
-          successfull.push(model);
-        }
-        next();
-      });
-    },
-    () => {
-      debug(
-        `job=closeAllModels status=done successfull=${
-          successfull.length
-        } failed=${
-          failed.length
-        } nextInvocation=${closeAllModelsJob.nextInvocation()}`
-      );
-
-      if (process.env.NODE_ENV === "production") {
-        let html = "<h2>Successfully closed the following models</h2><ul>";
-        successfull.forEach(model => {
-          html += `<li>${model}</li>`;
-        });
-        html += "</ul>";
-
-        if (failed.length > 0) {
-          html += "<h2>Closing of the following models failed</h2><ul>";
-          failed.forEach(model => {
-            html += `<li>${model}</li>`;
-          });
-          html += "</ul>";
-        }
-
-        const options = {
-          from: "noreply@medi.ch",
-          to: "devops@zebbra.ch",
-          subject: "Zeiterfassung-medi cronjob report",
-          html
-        };
-
-        app.models.Email.send(options, err => {
-          if (err) {
-            debug(err);
-          }
-        });
-      }
-    }
-  );
-};
-
-const yearTransition = () => {
+const yearTransition = async () => {
   const startOfYear = moment()
     .subtract(1, "year")
     .startOf("year");
@@ -146,6 +33,12 @@ const yearTransition = () => {
     closed: false
   };
 
+  while (app.booting) {
+    debug("============ APP BOOTING ============");
+    await sleep(100);
+  }
+  debug("============ APP STARTED ============");
+
   app.models.user.find(
     {
       include: {
@@ -157,9 +50,8 @@ const yearTransition = () => {
     },
     (err, dbUsers) => {
       if (err) {
-        debug(
-          `job=yearTransition status=failed error=${err} nextInvocation=${yearTransitionJob.nextInvocation()}`
-        );
+        debug(`job=yearTransition status=failed error=${err}`);
+        throw err;
       } else {
         const users = _.chain(dbUsers)
           .map(user => user.toJSON())
@@ -169,7 +61,7 @@ const yearTransition = () => {
           `job=yearTransition going to transfer count=${users.length} users`
         );
 
-        async.eachSeries(
+        Async.eachSeries(
           users,
           (user, next) => {
             reporting.timeseriesReporting(
@@ -217,6 +109,15 @@ const yearTransition = () => {
                           profile.id = dbEntry.id;
                         }
 
+                        debug(
+                          Object.assign({
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            username: user.username,
+                            ...profile
+                          })
+                        );
+
                         app.models["employment-profile"].upsert(
                           profile,
                           upserError => {
@@ -245,11 +146,7 @@ const yearTransition = () => {
           },
           () => {
             debug(
-              `job=yearTransition status=done successfull=${
-                successfull.length
-              } failed=${
-                failed.length
-              } nextInvocation=${yearTransitionJob.nextInvocation()}`
+              `job=yearTransition status=done successfull=${successfull.length} failed=${failed.length}`
             );
 
             if (process.env.NODE_ENV === "production") {
@@ -279,7 +176,10 @@ const yearTransition = () => {
                 if (mailErr) {
                   debug(mailErr);
                 }
+                process.exit(0);
               });
+            } else {
+              process.exit(0);
             }
           }
         );
@@ -288,16 +188,6 @@ const yearTransition = () => {
   );
 };
 
-// second minute hour day-of-month month day-of-week
-const closeAllModelsJob = schedule.scheduleJob("0 5 0 1 3 *", closeAllModels);
-debug(
-  `starting job=closeAllModels nextInvocation=${closeAllModelsJob.nextInvocation()}`
-);
-const yearTransitionJob = schedule.scheduleJob("0 5 0 1 1 *", yearTransition);
-debug(
-  `starting job=yearTransitionJob nextInvocation=${yearTransitionJob.nextInvocation()}`
-);
-const databaseBackupJob = schedule.scheduleJob("0 0 2 * * *", backup);
-debug(
-  `starting job=databaseBackupJob nextInvocation=${databaseBackupJob.nextInvocation()}`
-);
+if (require.main === module) {
+  yearTransition();
+}
